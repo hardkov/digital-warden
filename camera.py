dd06ec4bc8bacd62db452db9fbfa7fca174cbc7f
hardkov <krzysztofh150@interia.pl>
2021-05-04 20:14:53 +0200

    Added real time image uploading

--- camera.py
+++ camera.py
@@ -1,10 +1,14 @@
 import cv2
 import base64
+import threading
+from time import sleep
 
 from detection import detectHuman
 
+
 socket = None
 camera = None
+capturingBlocked = False
 currentHumanCount = 0
 
 def initializeCamera(socketio):
@@ -21,8 +25,14 @@ def closeCamera():
     camera.release()
     cv2.destroyAllWindows()
 
+def unlockCapture(timeout):
+    global capturingBlocked
+
+    sleep(timeout)
+    capturingBlocked = False
+
 def gen_frames():
-    global currentHumanCount
+    global currentHumanCount, capturingBlocked
 
     while True:
         success, frame = camera.read()
@@ -34,10 +44,12 @@ def gen_frames():
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
-        if count > currentHumanCount:
-            # save image and send to the client
-            print("imgFeed")
-            socket.emit("imgFeed", base64.b64encode(buffer))
+        if count > currentHumanCount and not capturingBlocked:
+            capturingBlocked = True
+            
+            socket.emit("imgFeed", base64.b64encode(buffer).decode("utf-8"))
+            
+            threading.Thread(target=unlockCapture, args=(2, )).start()
 
         currentHumanCount = count
 
