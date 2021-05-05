03d09b1f2d5264dca758f84f4abc2b9448e4edea
hardkov <krzysztofh150@interia.pl>
2021-05-05 22:38:38 +0200

    implemented sqlite storage

--- storage.py
+++ storage.py
@@ -1,8 +1,44 @@
-saved_frames = []
+import sqlite3
+import threading
+
+connection = None
+cursor = None 
+
+def initialize_database():
+    global connection, cursor
+
+    connection = sqlite3.connect("storage.db", check_same_thread=False)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='frames'")
+    if cursor.fetchone()[0] != 1:
+        cursor.execute('''CREATE TABLE frames (year number, month number, day number, hour number, src blob,
+                        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)''')
+
+        connection.commit()
 
 def save_frame(date, encoded_frame):
-    saved_frames.append({ "year": date.year, "month": date.month, "day": date.day, 
-        "hour": date.hour, "src": f"data:image/jpg;base64,{encoded_frame}"})
+    cursor.execute("INSERT INTO frames (year, month, day, hour, src) VALUES(?, ?, ?, ?, ?)", 
+                    (date.year, date.month, date.day, date.hour, f"data:image/jpg;base64,{encoded_frame}"))
 
-def get_saved_frames():
-    return saved_frames
+    connection.commit()
+
+def close_database():
+    connection.close()
+    connection = None
+    cursor = None
+
+def get_saved_frames(count):
+    if count == -1:
+        cursor.execute("SELECT * FROM frames ORDER BY time DESC") 
+    else:
+        cursor.execute(f"SELECT * FROM frames ORDER BY time DESC LIMIT {count}") 
+
+    result = cursor.fetchall()
+    result_objects = []
+
+    for row in result:
+        result_objects.append({ "year": row[0], "month": row[1], "day": row[2], 
+        "hour": row[3], "src": row[4]})
+
+    return result_objects
