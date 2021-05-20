24139d6694e842de9966dbdd00aa3e7bd0fb754c
hardkov <krzysztofh150@interia.pl>
2021-05-20 10:35:46 +0200

    Changed classifier to detect fullbody

--- detection.py
+++ detection.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2
  
-classifier = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+classifier = cv2.HOGDescriptor()
+classifier.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 def detect_human(frame):
     frame = cv2.resize(frame, (640, 480))
 
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    boxes = classifier.detectMultiScale(gray, 1.3, 5)
+    boxes, _ = classifier.detectMultiScale(gray, winStride=(8, 8))
 
     try:
         human_count = boxes.shape[0]
